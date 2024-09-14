e9a1dfb81af8a3b84d2e8eba20adbb8b61a9ca79
Mio Mahoney <mmahone5@oswego.edu>
2024-09-14 17:16:56 -0400

    Added History Endpoint

--- src/main/java/edu/oswego/hophacks/controller/Endpoints.java
+++ src/main/java/edu/oswego/hophacks/controller/Endpoints.java
@@ -1,15 +1,36 @@
 package edu.oswego.hophacks.controller;
 
+import edu.oswego.hophacks.object.Ontology;
 import org.json.simple.JSONObject;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+
 @RestController
 public class Endpoints {
+    HashMap<String, String> chatHistory;
+
     @PostMapping("/message")
     public ResponseEntity<JSONObject> sendMessage(@RequestBody JSONObject json) {
         return ResponseEntity.ok(json);
     }
+
+
+    //Question History
+    //When a doctor or patient reload the page the same information is displayed
+
+    /**
+     * Requests chat history
+     * @return JSON of Chat History
+     */
+
+    @GetMapping("/history")
+    public ResponseEntity<JSONObject> getHistory() {
+        return ResponseEntity.ok(new JSONObject(chatHistory));
+    }
+
 }
