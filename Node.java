d0b2293ce3af5cf8ab2c401be5d748f122bfb618
TheMostOGName <themostorigninalname@gmail.com>
2024-09-14 03:29:24 -0400

    Added more Node logic

--- Node.java
+++ Node.java
@@ -26,7 +26,7 @@ public class Node {
     }
 
     //Compare how many edges two nodes have in common
-    public static int compareEdges(Node a, Node b) throws InterruptedException {
+    public static int compareEdges(Node a, Node b) {
 
         ArrayList<Edge> edgesA = a.getEdges();
         ArrayList<Edge> edgesB = b.getEdges();
@@ -37,32 +37,93 @@ public class Node {
         int totalMatches = 0;
         //Loop through a
         for (int i = 0; i < edgesA.size(); i++) {
+            //System.out.println("Iteration " + i + "/" + edgesA.size());
             //get Edge A's destination URI
             String uriA = edgesA.get(i).getDest().getURI();
-            // System.out.println("Current A edge URI: " + uriA);
-            // System.out.println("Iteration: " + i + "/" + edgesA.size());
+            //System.out.println("Edge A DestID: " + edgesA.get(i).getDest().getID());
             //Loop through b. If a match is found, increment b and EXIT THE LOOP. 
             boolean loopAgain = true;
-            for (int j = 0; j < edgesB.size() || loopAgain; j++) {
+            for (int j = 0; j < edgesB.size() && loopAgain; j++) {
+                //System.out.println("Iteration " + j + "/" + edgesB.size());
                 //get Edge B's destination
-                String uriB = edgesB.get(i).getDest().getURI();
-                // System.out.println("Current B edge URI: " + uriB);
+                String uriB = edgesB.get(j).getDest().getURI();
+                //System.out.println("Edge B DestID: " + edgesB.get(j).getDest().getID());
                 if (uriA.equals(uriB)) {
                     //Found a match, quit loop
                     loopAgain = false;
                     //Increment total matches
                     totalMatches++;
-                    /* Uncomment if this method doesn't work
-                    System.out.println("Found match!");
-                    System.out.println("Looping again? " + loopAgain);
-                    System.out.println("Iteration: " + j + "/" + edgesB.size());
-                    System.out.println("Total Matches: " + totalMatches);
-                    TimeUnit.SECONDS.sleep(1);
-                     */
+                    //System.out.println("Found a match, total matches: " + totalMatches);
                 }
             }
         }
         return totalMatches;
+    }
+
+    //Same as compareEdges, except returns an ArrayList of all common edges between two Nodes.
+    //RETURNS THE EDGES FROM NODE A'S PERSPECTIVE!
+
+    public static ArrayList<Edge> commonEdges(Node a, Node b) {
+
+        ArrayList<Edge> edgesA = a.getEdges();
+        ArrayList<Edge> edgesB = b.getEdges();
+        if (compareEdges(a, b) == 0) {
+            return null;
+        }
+
+        ArrayList<Edge> matches = new ArrayList<Edge>();
+        //Loop through a
+        for (int i = 0; i < edgesA.size(); i++) {
+            //get Edge A's destination URI
+            String uriA = edgesA.get(i).getDest().getURI();
+            //Loop through b. If a match is found, increment b and EXIT THE LOOP.
+            boolean loopAgain = true;
+            for (int j = 0; j < edgesB.size() && loopAgain; j++) {
+                //get Edge B's destination
+                String uriB = edgesB.get(j).getDest().getURI();
+                if (uriA.equals(uriB)) {
+                    //Found a match, quit loop
+                    loopAgain = false;
+                    //Add a's current edge
+                    matches.add(edgesA.get(i));
+                }
+            }
+        }
+        return matches;
+    }
+
+
+    //Gets an ArrayList of Nodes and returns the pair of Nodes that share the most Edges
+    public static Node[] findOptimalPair(ArrayList<Node> list) {
+        Node[] output = new Node[2];
+        int currentMax = 0;
+
+
+        //Will always need a temporary arraylist without the current Node
+        ArrayList<Node> tempList = list;
+        //Loop through the arraylist in pairs, comparing edges for each of them. If the current pair is bigger than the currentMax,
+        //add that to the array of output Nodes.
+        for (int i = 0; i < list.size(); i++) {
+            Node iNode = list.get(i);
+            //System.out.println(iNode.getID());
+            //I hope this doesn't cause any issues since I'm getting the object from list rather than tempList
+            tempList.remove(iNode);
+            for (int j = 0; j < tempList.size(); j++) {
+                Node jNode = tempList.get(j);
+                //System.out.println(jNode.getID());
+                int currComp = compareEdges(iNode, jNode);
+                //System.out.println(currComp);
+                if (currComp > currentMax) {
+                    output[0] = iNode;
+                    output[1] = jNode;
+                    currentMax = currComp;
+                    //In the future, it may be a good idea to include numerous outcomes, like the second and third-likeliest options
+                }
+            }
+        }
+
+
+        return output;
     }
 
 }
--- Ontology.java
+++ Ontology.java
@@ -1,18 +1,57 @@
 import java.util.ArrayList; // import the ArrayList class
 
 public class Ontology {
-    public static void main(String[] args) throws InterruptedException {
-        ArrayList<Node> nodes = new ArrayList<Node>();
+    public static void main(String[] args)  {
+        ArrayList<Node> applicableConditions = new ArrayList<Node>();
 
         //Initialize a ton of nodes. If we're doing something simple it might be worth hard-coding this honestly
         Node conditions = new Node("Conditions", "URIPLACEHOLDER1");
-        Node condition1 = new Node("FirstCondition", "URIPLACEHOLDER2");
-        Node condition2 = new Node("SecondCondition", "URIPLACEHOLDER3");
+        Node fever = new Node("fever", "URIPLACEHOLDER2");
+        Node cough = new Node("cough", "URIPLACEHOLDER3");
         //Third layer node, condition -> condition1 -> condition11
-        Node condition11 = new Node("FirstCondition", "URIPLACEHOLDER4");
-        Edge e1 = new Edge(condition1, condition11, "Name", "URIPLACEHOLDER5");
-        Edge e2 = new Edge(condition2, condition11, "Name", "URIPLACEHOLDER6");
+        Node COVID = new Node("COVID", "URIPLACEHOLDER4");
+        Node skinInfection = new Node("SkinInfection", "URIPLACEHOLDER5"); //Fever only
+        Node commonCold = new Node("CommonCold", "URIPLACEHOLDER6");
+        Edge condition1 = new Edge(conditions, fever, "condition", "URIPLACEHOLDER10");
+        Edge condition2 = new Edge(conditions, cough, "condition", "URIPLACEHOLDER11");
+        Edge feverCause1 = new Edge(fever, skinInfection, "cause", "URIPLACEHOLDER12");
+        Edge feverCause2 = new Edge(fever, COVID, "Cause", "URIPLACEHOLDER13");
+        Edge coughCause1 = new Edge(cough, commonCold, "cause", "URIPLACEHOLDER14");
+        Edge coughCause2 = new Edge(cough, COVID, "Cause", "URIPLACEHOLDER15");
 
-        System.out.println("Node 1 and 2 common destination Nodes: " + Node.compareEdges(condition1, condition2));
+
+        //TO DO: Once nodes and edges are created, iterate through the condition nodes and ask questions to see which are applicable
+        //TO DO: Add applicable nodes to arraylist. Ensure we somehow store the answers to these questions so we can transmit them to PHC
+
+        //In this hard-coded case, assume both cases are applicable
+        applicableConditions.add(cough);
+        applicableConditions.add(fever);
+
+
+        //If there's multiple applicable conditions, find the two with the most common edges and spit out the possible causes
+        if (applicableConditions.size() > 1) {
+            Node[] mostCommonEdges = Node.findOptimalPair(applicableConditions);
+            if (mostCommonEdges[0] == null) {
+                //SHOULD NEVER HAPPEN, ONLY HERE FOR TESTING PURPOSES
+                System.out.println("Too much conflicting information, please try again.");
+            }
+
+            ArrayList<Edge> commonEdges = Node.commonEdges(mostCommonEdges[0], mostCommonEdges[1]);
+
+            System.out.println("Most likely diagnosis: ");
+            for (int i = 0; i < commonEdges.size(); i++) {
+                System.out.println((i + 1) + ". " + commonEdges.get(i).getDest().getID());
+            }
+        }
+        //If there's one applicable condition, spit out all possible causes
+        else if (applicableConditions.size() == 1){
+            Node condition = applicableConditions.getFirst();
+            System.out.println("Most likely diagnosis: ");
+            for (int i = 0; i < condition.getEdges().size(); i++) {
+                System.out.println(i + ". " + condition.getEdges().get(i).getDest().getID());
+            }
+        } else {
+            System.out.println("Patient appears to have no applicable conditions.");
+        }
     }
 }
